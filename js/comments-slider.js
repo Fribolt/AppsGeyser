7328190e542d0843f114103ad5c31e17d66c9a0d
Vlad Prybylnov <pribilnov97@gmail.com>
2018-08-06 22:48:25 +0300

    added mobile version comment slider

--- js/comments-slider.js
+++ js/comments-slider.js
@@ -1,35 +1,71 @@
-const sliderState = {
+var sliderState = {
   activeSlide: 2
 };
 
-const stringToNumber = function (string) {
+var containerSize = document.querySelector('.slider-container').offsetWidth;
+var elementSize = document.querySelector('.slider .slide').offsetWidth;
+var spaceBetween = (containerSize - elementSize) / 2;
+var initialLeft = spaceBetween - elementSize;
+
+var leftEnd = initialLeft + elementSize;
+var midleEnd = initialLeft;
+var rightEnd = initialLeft - elementSize;
+
+var moveSlider = function (index) {
+    if ( index === 1 ) {
+        document.querySelector('.slider').style.left = leftEnd  + 'px';
+    } else if ( index === 2 ) {
+        document.querySelector('.slider').style.left = midleEnd + 'px';
+    } else if ( index === 3 ) {
+        document.querySelector('.slider').style.left = rightEnd + 'px';
+    }
+};
+
+
+moveSlider(sliderState.activeSlide);
+
+window.onresize = function() {
+    containerSize = document.querySelector('.slider-container').offsetWidth;
+    elementSize = document.querySelector('.slider .slide').offsetWidth;
+    spaceBetween = (containerSize - elementSize) / 2;
+    initialLeft = spaceBetween - elementSize;
+
+    leftEnd = initialLeft + elementSize;
+    midleEnd = initialLeft;
+    rightEnd = initialLeft - elementSize;
+    moveSlider(sliderState.activeSlide);
+};
+
+var stringToNumber = function (string) {
     return + string
 };
 
-const moveSlider = function (distance) {
-    document.querySelector('.slider').style.left = distance + 'px';
-};
-
-const showComment = function (index) {
+var showComment = function (index) {
     document.querySelector('.comments .comment:nth-child(' + sliderState.activeSlide + ')').classList.toggle('active');
     document.querySelector('.comments .comment:nth-child(' + index + ')').classList.toggle('active');
 };
 
-const setActiveSlide = function (index) {
+var setActiveSlide = function (index) {
     document.querySelector('.slider .slide:nth-child(' + sliderState.activeSlide + ')').classList.toggle('active');
     document.querySelector('.slider .slide:nth-child(' + index + ')').classList.toggle('active');
 };
 
-const setActiveIndicator = function (index) {
+var setActiveIndicator = function (index) {
     document.querySelector('.indicators .indicator:nth-child(' + sliderState.activeSlide + ')').classList.toggle('active');
     document.querySelector('.indicators .indicator:nth-child(' + index + ')').classList.toggle('active');
 
 };
 
 const changeSlide = function (index) {
-    const elementSize = document.querySelector('.slider .slide').offsetWidth;
-    const stepSize = - elementSize * (index - 1);
-    moveSlider(stepSize);
+    var containerSize = document.querySelector('.slider-container').offsetWidth;
+    var elementSize = document.querySelector('.slider .slide').offsetWidth;
+    var spaceBetween = (containerSize - elementSize) / 2;
+    // console.log('containerSize' + containerSize);
+    // console.log('elementSize' + elementSize);
+    // console.log('spaceBetween' + spaceBetween);
+    console.log('initialLeft' + initialLeft);
+
+    moveSlider(index);
     showComment(index);
     setActiveSlide(index);
     setActiveIndicator(index);
